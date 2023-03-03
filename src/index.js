b8e307fe245d18277509399bbaff142588f64a13
exorbist1 <48337920+exorbist1@users.noreply.github.com>
2023-03-03 13:20:09 +1100

    revert to start

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import axios from 'axios';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,12 +16,3 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-axios.get('https://jsonplaceholder.typicode.com/posts', {
-	headers: {
-		'Access-Control-Allow-Origin': "*", 
-	}
-}).then(resp => {
-
-    console.log(resp.data);
-});
