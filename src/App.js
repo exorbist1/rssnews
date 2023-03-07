592ccff62f26204fcebedba5d797cea0e6add2f9
exorbist1 <48337920+exorbist1@users.noreply.github.com>
2023-03-07 17:12:00 +1100

    fetch
    fetching data from news.api

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 import Navbar from './components/Navbar';
 import SearchStats from './components/SearchStats';
 import Card from './components/Card';
-
 const data = require('./components/data.json')
 
-function App() {
 
+function App() {
+	fetch('https://newsapi.org/v2/everything?q=f1&language=en&from=2023-03-04&apiKey=730160677c7746fb9d3d7900db5990a1')
+		.then(response => response.json())
+  		.then(data => console.log(data.articles));
+	
+	
 	const cards = (data.articles).map(item =>{
 		return( 
 			<Card
