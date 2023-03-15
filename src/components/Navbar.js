4fa2e256b3a6f7d411b83c54b9d738898bb31229
exorbist1 <48337920+exorbist1@users.noreply.github.com>
2023-03-15 16:44:06 +1100

    navbar_form
    inclusion of form for search in the navbar section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,12 @@ export default function Navbar(){
     return(
         <nav className="navbar">
             <h1> World Wide News</h1>
+            <div className="search--container">
+                <form action="">
+                    <input type="search" placeholder="Search.." name="search" />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
         </nav>
     )
 }
