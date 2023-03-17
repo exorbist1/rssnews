7dc1a6d7a61570cdbb5a0e80fca15313e982994d
exorbist1 <48337920+exorbist1@users.noreply.github.com>
2023-03-17 16:10:19 +1100

    dynamic_search
    dynamic search implemented with API in app.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,58 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import SearchStats from './components/SearchStats';
 import Card from './components/Card';
 import WithCardsLoading from './components/withCardsLoading';
-// const data = require('./components/data.json')
 
 
 function App() {
-	const CardsLoading = WithCardsLoading(Card);
-	const [appState, setAppState] = useState({
+	const [searchValue, setSearchValue] = useState({
 		loading: false, 
 		articles: null,
 		status: null,
 		results: null,
 	});
 
-	useEffect(() => {
-		setAppState({ loading: true });
-		const apiUrl = `https://newsapi.org/v2/everything?q=f1&language=en&from=2023-03-04&apiKey=730160677c7746fb9d3d7900db5990a1`;
+	const CardsLoading = WithCardsLoading(Card);
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		console.log(searchValue);
+
+		const apiUrl = `https://newsapi.org/v2/everything?q=${searchValue}}&language=en&from=2023-03-15&apiKey=730160677c7746fb9d3d7900db5990a1`;
 		fetch(apiUrl)
-		  .then((res) => res.json())
-		  .then((res) => {
-			setAppState({ loading: false, articles: res.articles, status: res.status, results: res.totalResults});
-		  })
-	  }, [setAppState]);
-	
-	console.log(appState.results)
+		  	.then((res) => res.json())
+		  	.then((res) => {
+				setSearchValue({ loading: false, articles: res.articles, status: res.status, results: res.totalResults});
+		  	})
+		
+	}
 
   	return (
 		<div>
 			<Navbar />
+
+			<div className="search--container">
+                <form onSubmit={handleSubmit} className="search--form">
+                    <input 
+						type="text" 
+						placeholder="Search.." 
+						className="search--bar" 
+						value= {searchValue}
+						onChange = { (event) =>
+							setSearchValue(event.target.value)
+						}	
+					/>
+                    <button type="submit" className="search--button">Search</button>
+                </form>
+            </div>
+
 			<SearchStats 
-				status = {appState.status}
-				number = {appState.results}
+				status = {searchValue.status}
+				number = {searchValue.results}
 			/>
-			<CardsLoading isLoading={appState.loading} articles={appState.articles} />
+			<CardsLoading isLoading={searchValue.loading} articles={searchValue.articles} />
     	</div>
   	);
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,12 @@ export default function Navbar(){
     return(
         <nav className="navbar">
             <h1> World Wide News</h1>
-            <div className="search--container">
-                <form action="">
-                    <input type="search" placeholder="Search.." name="search" />
-                    <button type="submit">Submit</button>
+            {/* <div className="search--container">
+                <form action="" className="search--form">
+                    <input type="search" placeholder="Search.." className="search--bar" />
+                    <button type="submit" className="search--button">Search</button>
                 </form>
-            </div>
+            </div> */}
         </nav>
     )
 }
